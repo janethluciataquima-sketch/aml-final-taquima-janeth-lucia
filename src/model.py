09d1074b229e054cc22465390ef789e36eb9be11
Janeth Lucía Taquima <ltaquima@pucp.edu.pe>
2026-05-01 23:11:24 -0500

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,22 @@
+
+import torch.nn as nn
+
+class NubeAndinaMLP(nn.Module):
+    def __init__(self, input_size):
+        super(NubeAndinaMLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
